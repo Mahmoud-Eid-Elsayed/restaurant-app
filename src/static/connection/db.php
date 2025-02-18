81ff4e2e5ec720bac8368640703a0841816b03c6
Mado007 <mahmodeid2019@gmail.com>
2025-02-18 16:17:01 +0200

    fix database connection

--- src/static/connection/db.php
+++ src/static/connection/db.php
@@ -1,9 +1,9 @@
 <?php
 $host = 'localhost';
-$port = '3306'; 
+$port = 'add-your-port-number';
 $dbname = 'Restaurant_DB';
 $username = 'root';
-$password = 'Smsm@012';
+$password = '';
 
 try {
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
